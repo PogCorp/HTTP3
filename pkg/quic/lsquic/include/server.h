1a3a246d760be551b57e862a77d4020bf14c0be2
HikaruTashiro <hikarutashiro@hotmail.com>
2024-09-07 22:23:43 -0300

    feat: add ALPN setter and server initialization and cleanup
    - add_alpn method to insert proto for SSL aplication layer negotiation
    - add configuration of lsquic engine depending on proto passed
    - server cleanup to remove all events registerd and memory allocated

--- pkg/quic/lsquic/include/server.h
+++ pkg/quic/lsquic/include/server.h
@@ -1,6 +1,7 @@
 #include "openssl/base.h"
 #include <ev.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <sys/queue.h>
 #include <sys/socket.h>
 
@@ -10,7 +11,6 @@
 #pragma once
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MAX_OUT_BATCH_SIZE 1024
 #define MAX_OUT_BATCH_SIZE 1024 // DOCS: max size of lsquic out packets. Defined in  https://lsquic.readthedocs.io/en/latest/internals.html#out-batch
 #define MAX_PACKET_SIZE 65535
 #define NDROPPED_SIZE CMSG_SPACE(sizeof(uint32_t)) // get a platform independent size of ancillary field for dropped packets
@@ -20,33 +20,28 @@
 // total size for ancillary. (just the sum of the previous)
 #define CMSG_SIZE (CMSG_SPACE(MAX(IPV4_DST_MSG_SIZE, IPV6_DST_MSG_SIZE) + NDROPPED_SIZE + ECN_SIZE))
 
+#define ALPN_LEN 0x100
+
 enum ReadStatus {
     OK,
     NO_ROOM,
     ERROR,
 };
 
-enum IpVersion {
-    IPV4,
-    IPV6
-};
+enum IpVersion { IPV4,
+    IPV6 };
 
 struct v_server;
 struct packet_buffer;
 
 TAILQ_HEAD(v_servers, v_server);
 
-// TODO: write an interface type to comunicate with Go
 typedef struct server {
     ev_timer time_watcher;
     struct ev_loop* event_loop;
-    struct sockaddr_storage local_address;
-    struct lsquic_stream_if stream_callbacks;
-    struct lsquic_engine_settings settings;
     SSL_CTX* ssl_ctx;
     lsquic_engine_t* quic_engine;
-    char* keylog_path;
-    char alpn[0x100];
+    char alpn[ALPN_LEN];
     struct lsquic_hash* certificates;
     struct v_servers* v_servers;
     // interface QuicAdapter adapter_callbacks;
@@ -62,12 +57,13 @@ struct v_server {
     enum IpVersion ip_ver;
     struct packet_buffer* buffer;
     ev_io socket_watcher;
+    uint32_t dropped_packets;
 };
 
 struct packet_buffer {
     unsigned char* buffer_data;
     unsigned char* cmsg_data;
-    struct msghdr* packets;
+    struct mmsghdr* packets;
     struct sockaddr_storage* local_addresses;
     struct sockaddr_storage* peer_addresses;
     struct iovec* vecs;
@@ -76,14 +72,23 @@ struct packet_buffer {
     unsigned buffer_size;
 };
 
-typedef struct {
+struct lsquic_stream_ctx {
     // total size of payload
-    size_t total_size;
+    size_t rcv_total_size;
     // offset of written/read payload
-    off_t offset;
+    off_t rcv_offset;
     // payload data
-    unsigned char* buffer;
-} server_stream_ctx;
+    char* rcv_buffer;
+    // used in conjunction to memstream to accumulate client data
+    FILE* file_handler;
+
+    size_t snd_total_size;
+    off_t snd_offset;
+    char* snd_buffer;
+
+    lsquic_stream_t* stream;
+    struct v_server* v_server;
+};
 
 /* TODO: add Cgo callbacks */
 
@@ -93,31 +98,49 @@ typedef struct {
  * consumer must check errno to ensure that the configuration was appropriate
  * and server can listen
  * */
-void newServer(Server* server, const char* keylog);
+void new_server(
+    Server* server,
+    const char* keylog,
+    char** alpn_protos,
+    size_t* alpn_str_size,
+    int alpn_proto_len);
+
+void server_cleanup(Server* server);
+
+bool server_prepare(Server* server);
+
+bool server_listen(Server* server);
 
 /*
  *
  * */
-bool add_v_server(Server* server, const char* uri,
-    const char* certkey, const char* keyfile);
+bool add_v_server(
+    Server* server,
+    const char* uri,
+    const char* certkey,
+    const char* keyfile);
 
 struct packet_buffer* new_packet_buffer(int fd);
 
+void packet_buffer_cleanup(struct packet_buffer* buffer);
+
 bool v_server_configure_socket(struct v_server* v_server);
 
-void server_read_socket(EV_P_ ev_io* w, int revents);
-
-/* connection methods */
-static int server_packets_out(void* packets_out_ctx,
+/* Connection methods */
+static int server_write_socket(
+    void* packets_out_ctx,
     const struct lsquic_out_spec* specs,
     unsigned count);
+
+void server_read_socket(EV_P_ ev_io* w, int revents);
 
 /* LSQUIC Callbacks */
 
 /*
  * Callback to process the event of a new connection to the server
  * */
-lsquic_conn_ctx_t* server_on_new_connection(void* stream_if_ctx,
+lsquic_conn_ctx_t* server_on_new_connection(
+    void* stream_if_ctx,
     struct lsquic_conn* conn);
 
 /*
@@ -125,11 +148,18 @@ lsquic_conn_ctx_t* server_on_new_connection(void* stream_if_ctx,
  * */
 void server_on_closed_connection(lsquic_conn_t* conn);
 
-lsquic_stream_ctx_t* server_on_new_stream(void* stream_if_ctx,
+lsquic_stream_ctx_t* server_on_new_stream(
+    void* stream_if_ctx,
     struct lsquic_stream* stream);
 
-void server_on_read(struct lsquic_stream* stream, lsquic_stream_ctx_t* h);
+void server_on_read(
+    struct lsquic_stream* stream,
+    lsquic_stream_ctx_t* stream_ctx);
 
-void server_on_write(struct lsquic_stream* stream, lsquic_stream_ctx_t* h);
+void server_on_write(
+    struct lsquic_stream* stream,
+    lsquic_stream_ctx_t* stream_ctx);
 
-void server_on_close(struct lsquic_stream* stream, lsquic_stream_ctx_t* h);
+void server_on_close(
+    struct lsquic_stream* stream,
+    lsquic_stream_ctx_t* stream_ctx);
