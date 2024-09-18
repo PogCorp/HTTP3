62f781f6d5b5b27c768f2fb46e8bc6c8596b2fa2
HikaruTashiro <hikarutashiro@hotmail.com>
2024-09-17 21:48:57 -0300

    feat: add echo demo for lsquic adapter

---
+++ pkg/quic/lsquic/cmd/echo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	lsquic "poghttp3/pkg/quic/lsquic"
+)
+
+func main() {
+	api := lsquic.NewLsQuicApi()
+	lsquicServer, err := lsquic.NewLsquicServer("localhost:8080", "../certs/priv.key", "../certs/cert.crt", api)
+	if err != nil {
+		log.Println("failed to start lsquic server", err)
+		return
+	}
+	if err := lsquicServer.Listen(); err != nil {
+		log.Println("failed on server listen", err)
+	}
+}
