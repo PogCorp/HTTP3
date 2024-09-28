fde78b4b72afb6872fe2ca14c2d27ba236bda216
HikaruTashiro <hikarutashiro@hotmail.com>
2024-09-27 21:44:06 -0300

    reafactor: moving to new adapter interface
    - removes creation of mock certificate and adds propper loader of
      certificate
    - removes old Connection interface and adds QuicCID in it's place

--- pkg/quic/quicgo/provider.go
+++ pkg/quic/quicgo/provider.go
@@ -2,64 +2,54 @@ package quicgo
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
+	"log"
+	adapter "poghttp3/pkg/quic"
+	"time"
+
 	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/logging"
 	"github.com/quic-go/quic-go/qlog"
-	"log"
-	"math/big"
-	"os"
-	"path/filepath"
-	adapter "poghttp3/pkg/quic"
-	"time"
 )
 
 const addr = "localhost:4242"
 
-type QuicGoAdapter struct {
-	Streams map[int64]adapter.QuickStream
+type quicServer struct {
+	quicApi   adapter.QuicAPI
+	tlsConfig *tls.Config
+	sni       string
 }
 
-func NewQuicGoAdapter() adapter.QuicAdapter {
-	return &QuicGoAdapter{
-		Streams: make(map[int64]adapter.QuickStream),
-	}
-}
-
-type quickServer struct {
-	adapter adapter.QuicAdapter
-}
-
-func NewQuickGoServer(adapter adapter.QuicAdapter) adapter.QuickServer {
-	return &quickServer{
-		adapter: adapter,
-	}
-}
-
-func (q *quickServer) Listen() error {
-	qlogFilename := fmt.Sprintf("server_%s.qlog", time.Now().Format("20060102_150405"))
-	qlogFile, err := os.Create(filepath.Join(".", qlogFilename))
+func NewQuicGoServer(uri, keyfile, certfile string, api adapter.QuicAPI) (adapter.QuicServer, error) {
+	certficate, err := tls.LoadX509KeyPair(certfile, keyfile)
 	if err != nil {
-		return fmt.Errorf("failed to create qlog file: %v", err)
+		return nil, err
 	}
-	defer qlogFile.Close()
+
+	return &quicServer{
+		quicApi:   api,
+		tlsConfig: &tls.Config{Certificates: []tls.Certificate{certficate}},
+		sni:       "TODO",
+	}, nil
+}
+
+func (q *quicServer) Listen() error {
 
 	config := &quic.Config{
 		EnableDatagrams: true,
-		Tracer: func(ctx context.Context, p logging.Perspective, ci quic.ConnectionID) *logging.ConnectionTracer {
-			//q.ConnectionIdChan <- ci.String()
-			q.adapter.OnNewConnection(ci.String())
-			return qlog.NewConnectionTracer(qlogFile, p, ci)
+		Tracer: func(ctx context.Context, p logging.Perspective, cid quic.ConnectionID) *logging.ConnectionTracer {
+			quicGoCid := NewQuicGoCID(cid)
+			q.quicApi.OnNewConnection(quicGoCid)
+			return qlog.DefaultConnectionTracer(ctx, p, cid)
 		},
 		MaxIdleTimeout: time.Minute * 30,
+		GetConfigForClient: func(info *quic.ClientHelloInfo) (*quic.Config, error) {
+			return nil, nil
+		},
 	}
 
-	listener, err := quic.ListenAddr(addr, generateTLSConfig(), config)
+	listener, err := quic.ListenAddr(addr, q.tlsConfig, config)
 	if err != nil {
 		return err
 	}
@@ -77,48 +67,7 @@ func (q *quickServer) Listen() error {
 	}
 }
 
-func (q *QuicGoAdapter) consumeConnectionID() {
-	// for value := range q.ConnectionIdChan {
-	// 	fmt.Printf("Received ConnectionID: %s\n", value)
-	// 	conn := <-q.ConnectionChan
-	// 	fmt.Printf("Received Conn\n")
-	// 	q.Connections[value] = NewConnection(conn, value)
-	// 	fmt.Println("Connection registered\n")
-	// 	q.OnNewConnection(q.Connections[value])
-	// 	// go q.handleConnection(q.Connections[value])
-	// }
-}
-
-func (q *QuicGoAdapter) OnNewStream(streamID int64) {
-	fmt.Printf("Accepted stream with id: %d\n", streamID)
-}
-
-func (q *QuicGoAdapter) OnReadStream(streamID int64, data []byte) {
-	fmt.Printf("Read stream: %d with content: %s\n", streamID, string(data))
-}
-
-func (q *QuicGoAdapter) OnCancelledConnection(connectionID string) {
-	fmt.Printf("Connection with connection id: %s was cancelled\n", connectionID)
-}
-
-func (q *QuicGoAdapter) OnNewConnection(connectionID string) {
-	fmt.Printf("Received ConnectionID: %s\n", connectionID)
-
-	// defer conn.CloseWithError(0, "connection closed")
-	//
-	// for {
-	// 	stream, err := conn.AcceptStream(context.Background())
-	// 	if err != nil {
-	// 		log.Printf("Failed to accept stream: %v", err)
-	// 		return
-	// 	}
-	//
-	// 	fmt.Printf("Handling stream from connection id: %s\n", conn.ConnectionID())
-	// 	go conn.OnNewStream(stream)
-	// }
-}
-
-func (q *quickServer) handleConnection(ctx context.Context, conn quic.Connection) {
+func (q *quicServer) handleConnection(ctx context.Context, conn quic.Connection) {
 	for {
 		stream, err := conn.AcceptStream(ctx)
 		if err != nil {
@@ -126,15 +75,16 @@ func (q *quickServer) handleConnection(ctx context.Context, conn quic.Connection
 			// TODO: what to do when error
 			return
 		}
-		q.adapter.OnNewStream(int64(stream.StreamID()))
+		quicGoStream := NewStream(stream)
+		q.quicApi.OnNewStream(quicGoStream)
 		go q.handleStream(ctx, stream)
 	}
 }
 
-func (q *quickServer) handleStream(ctx context.Context, stream quic.Stream) {
+func (q *quicServer) handleStream(ctx context.Context, stream quic.Stream) {
 	defer stream.Close()
-	streamID := int64(stream.StreamID())
-	q.adapter.OnNewStream(streamID)
+	quicGoStream := NewStream(stream)
+	q.quicApi.OnNewStream(quicGoStream)
 
 	buf := make([]byte, 4096)
 
@@ -144,125 +94,15 @@ func (q *quickServer) handleStream(ctx context.Context, stream quic.Stream) {
 	}
 
 	fmt.Printf("Received DATA: %s\n", string(buf))
-	q.adapter.OnReadStream(streamID, buf)
+	q.quicApi.OnReadStream(quicGoStream, buf)
 	q.simpleWrite("This is a response from server", stream)
 }
 
-func (q *quickServer) simpleWrite(data string, stream quic.Stream) {
+func (q *quicServer) simpleWrite(data string, stream quic.Stream) {
 	if _, err := stream.Write([]byte(data)); err != nil {
 		log.Printf("Failed to write to stream: %v", err)
 		return
 	}
 
 	fmt.Printf("Server sent: '%s'\n", data)
-}
-
-type quicConnection struct {
-	conn         quic.Connection
-	connectionID string
-}
-
-func NewConnection(conn quic.Connection, connectionID string) adapter.Connection {
-	return &quicConnection{
-		conn:         conn,
-		connectionID: connectionID,
-	}
-}
-
-func (qc *quicConnection) OnReadStream(stream adapter.Stream) ([]byte, error) {
-	buf := make([]byte, 4096)
-
-	if _, err := stream.Read(buf); err != nil {
-		log.Printf("Failed to read from stream: %v", err)
-		return nil, err
-	}
-
-	fmt.Printf("Received DATA: %s\n", string(buf))
-
-	return buf, nil
-}
-
-func (qc *quicConnection) OnWriteStream(stream adapter.Stream, payload []byte) error {
-	if _, err := stream.Write([]byte(payload)); err != nil {
-		log.Printf("Failed to write to stream: %v", err)
-		return err
-	}
-
-	fmt.Printf("Server sent: '%s'\n", payload)
-	return nil
-}
-
-func (qc *quicConnection) OnNewStream(stream adapter.Stream) {
-	defer stream.Close()
-	receivedPayload, err := qc.OnReadStream(stream)
-	if err != nil {
-		return
-	}
-
-	if err := qc.OnWriteStream(stream, receivedPayload); err != nil {
-		return
-	}
-}
-
-func (qc *quicConnection) ConnectionID() string {
-	return qc.connectionID
-}
-
-func (qc *quicConnection) AcceptStream(ctx context.Context) (adapter.Stream, error) {
-	stream, err := qc.conn.AcceptStream(ctx)
-	if err != nil {
-		log.Printf("Failed to accept stream: %v", err)
-		return nil, err
-	}
-
-	return NewStream(stream), nil
-}
-
-func (qc *quicConnection) CloseWithError(applicationErrorCode uint64, errorMessage string) error {
-	return qc.CloseWithError(applicationErrorCode, errorMessage)
-}
-
-type quicStream struct {
-	stream quic.Stream
-}
-
-func NewStream(stream quic.Stream) adapter.Stream {
-	return &quicStream{
-		stream: stream,
-	}
-}
-
-func (qs *quicStream) Read(buf []byte) (n int, err error) {
-	return qs.stream.Read(buf)
-}
-
-func (qs *quicStream) Write(p []byte) (n int, err error) {
-	return qs.stream.Write(p)
-}
-
-func (qs *quicStream) Close() error {
-	return qs.stream.Close()
-}
-
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"quic-echo-example"},
-	}
 }
