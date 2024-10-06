ce09ef7318efcd3b3f2caa04297b6e18657686fa
rasteiro11 <titico0306@gmail.com>
2024-10-06 10:10:50 -0300

    feat: add qpack encoder with adapter

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	adapter "poghttp3/pkg/qpack"
+	quicGoAdapter "poghttp3/pkg/qpack/quicgo"
+)
+
+func main() {
+	encoder := quicGoAdapter.NewQuicGoQpackEncoder()
+
+	headers := []adapter.HeaderField{
+		{Name: ":method", Value: "GET"},
+		{Name: ":scheme", Value: "https"},
+		{Name: ":path", Value: "/index.html"},
+		{Name: ":authority", Value: "www.example.com"},
+		{Name: "accept", Value: "text/html"},
+		{Name: "user-agent", Value: "Go-http-client/1.1"},
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	if err := encoder.Encode(buf, headers...); err != nil {
+		fmt.Printf("[encoder.Encode] returned error: %+v\n", err)
+		return
+	}
+
+	fmt.Printf("Encoded Headers: %x\n", buf.Bytes())
+
+	headerFields, err := encoder.Decode(buf)
+	if err != nil {
+		fmt.Printf("[encoder.Decode] returned error: %+v\n", err)
+		return
+	}
+
+	for _, headerField := range headerFields {
+		fmt.Printf("%s: %s\n", headerField.Name, headerField.Value)
+	}
+}
