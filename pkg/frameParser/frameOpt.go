fb363bddbc2bc17912d9af864ca3475e0e9e7fbc
rasteiro11 <titico0306@gmail.com>
2024-10-20 16:18:06 -0300

    refactor: frame parser

--- pkg/frameParser/frameOpt.go
+++ pkg/frameParser/frameOpt.go
@@ -1,4 +1,4 @@
-package main
+package frameparser
 
 import (
 	"bytes"
--- pkg/frameParser/frameParser.go
+++ pkg/frameParser/frameParser.go
@@ -1,4 +1,4 @@
-package main
+package frameparser
 
 import (
 	"encoding/binary"
@@ -39,68 +39,17 @@ func NewFrameParser(r io.Reader, opts ...FrameParserOption) *FrameParser {
 	return frameParser
 }
 
-// read stream and parse multiple frames
-func (p *FrameParser) ParseFrames() error {
-	for {
-		// read the frame type (the first byte)
-		var frameType uint8
-		if err := binary.Read(p.reader, binary.BigEndian, &frameType); err != nil {
-			if errors.Is(err, io.EOF) {
-				fmt.Println("End of File Received")
-				return nil
-			}
-			return fmt.Errorf("failed to read byte data, got err: %w", err)
+func (p *FrameParser) readFrameType() (uint8, error) {
+	var frameType uint8
+	if err := binary.Read(p.reader, binary.BigEndian, &frameType); err != nil {
+		if errors.Is(err, io.EOF) {
+			fmt.Println("End of File Received")
+			return 0, nil
 		}
-
-		// read the length field
-		length, err := p.readFrameLength()
-		if err != nil {
-			return fmt.Errorf("unable to acquire frame lenght, got err: %w", err)
-		}
-
-		// read the payload according to its length
-		payload := make([]byte, length)
-		if _, err := io.ReadFull(p.reader, payload); err != nil {
-			return fmt.Errorf("failed to read frame data, got err: %w", err)
-		}
-
-		// parse frame according to it's type
-		switch frameType {
-		case FrameTypeHeaders:
-			headersFrame := &HeadersFrame{}
-			if err := headersFrame.Decode(payload); err != nil {
-				return fmt.Errorf("failed to decode HeadersFrame, got err: %w", err)
-			}
-			fmt.Printf("HEADERS Frame decodificado: %s\n", headersFrame.Headers)
-
-			headerFields, err := p.qpackDecoder.Decode(headersFrame.Headers)
-			if err != nil {
-				fmt.Printf("[encoder.Decode] returned error: %+v\n", err)
-				return fmt.Errorf("failed to decode headers using qpack decoder, got err: %w", err)
-			}
-
-			for _, hf := range headerFields {
-				fmt.Printf("KEY: %s VALUE: %s\n", hf.Name, hf.Value)
-			}
-
-		case FrameTypeData:
-			dataFrame := &DataFrame{}
-			if err := dataFrame.Decode(payload); err != nil {
-				return fmt.Errorf("failed to decode DataFrame, got err: %w", err)
-			}
-			fmt.Printf("Decoded DataFrame: %s\n", dataFrame.Data)
-
-		case FrameTypeSettings:
-			settingsFrame := &SettingsFrame{}
-			if err := settingsFrame.Decode(payload); err != nil {
-				return fmt.Errorf("failed to decode SettingsFrame, got err: %w", err)
-			}
-			fmt.Println("SettingsFrame decoded")
-
-		default:
-			return fmt.Errorf("unknown frame Type: %d", frameType)
-		}
+		return 0, fmt.Errorf("failed to read byte data, got err: %w", err)
 	}
+
+	return frameType, nil
 }
 
 func (p *FrameParser) readFrameLength() (uint64, error) {
@@ -109,4 +58,70 @@ func (p *FrameParser) readFrameLength() (uint64, error) {
 		return 0, err
 	}
 	return uint64(length), nil
+}
+
+// read stream and parse multiple frames
+func (p *FrameParser) ParseNextFrame() (Frame, error) {
+	// read the frame type (the first byte)
+	frameType, err := p.readFrameType()
+	if err != nil {
+		return nil, err
+	}
+
+	// read the length field
+	length, err := p.readFrameLength()
+	if err != nil {
+		return nil, fmt.Errorf("unable to acquire frame lenght, got err: %w", err)
+	}
+
+	// read the payload according to its length
+	payload := make([]byte, length)
+	if _, err := io.ReadFull(p.reader, payload); err != nil {
+		return nil, fmt.Errorf("failed to read frame data, got err: %w", err)
+	}
+
+	// parse frame according to it's type
+	switch frameType {
+	case FrameTypeHeaders:
+		headersFrame := &HeadersFrame{}
+		if err := headersFrame.Decode(payload); err != nil {
+			return nil, fmt.Errorf("failed to decode HeadersFrame, got err: %w", err)
+		}
+		fmt.Printf("HEADERS Frame decodificado: %s\n", headersFrame.Headers)
+
+		headerFields, err := p.qpackDecoder.Decode(headersFrame.Headers)
+		if err != nil {
+			fmt.Printf("[encoder.Decode] returned error: %+v\n", err)
+			return nil, fmt.Errorf("failed to decode headers using qpack decoder, got err: %w", err)
+		}
+
+		for _, hf := range headerFields {
+			fmt.Printf("KEY: %s VALUE: %s\n", hf.Name, hf.Value)
+		}
+
+		return headersFrame, nil
+	case FrameTypeData:
+		dataFrame := &DataFrame{}
+		if err := dataFrame.Decode(payload); err != nil {
+			return nil, fmt.Errorf("failed to decode DataFrame, got err: %w", err)
+		}
+		fmt.Printf("Decoded DataFrame: %s\n", dataFrame.Data)
+
+		return dataFrame, nil
+	case FrameTypeSettings:
+		settingsFrame := &SettingsFrame{}
+		if err := settingsFrame.Decode(payload); err != nil {
+			return nil, fmt.Errorf("failed to decode SettingsFrame, got err: %w", err)
+		}
+		fmt.Println("SettingsFrame decoded")
+
+		return settingsFrame, nil
+	default:
+		// NOTE: in this case we have received unknown frames so discard it
+		if _, err := io.CopyN(io.Discard, p.reader, int64(length)); err != nil {
+			return nil, err
+		}
+
+		return nil, fmt.Errorf("unknown frame Type: %d", frameType)
+	}
 }
