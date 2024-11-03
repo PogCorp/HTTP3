e26ed9a4bbef23be328ecdd61edad4c0975984d1
HikaruTashiro <hikarutashiro@hotmail.com>
2024-11-03 15:35:26 -0300

    feat(body): adding validations to the request body

--- pkg/http3/requestBody/requestBody.go
+++ pkg/http3/requestBody/requestBody.go
@@ -1,27 +1,45 @@
 package requestbody
 
 import (
+	"fmt"
 	"io"
 	"poghttp3/pkg/http3"
 	adapter "poghttp3/pkg/quic"
+	"sync"
 )
 
 type requestBody struct {
-	biStream      adapter.QuicBiStream
-	stream        io.Reader
-	contentLength int
-	bytesToBeRead int
+	biStream adapter.QuicBiStream
+	stream   io.Reader
+	// invariant, contentLength must be positive
+	contentLength    int
+	bytesToBeRead    int
+	violationHandler func()
 }
 
 var _ RequestBody = (*requestBody)(nil)
 
-func NewRequestBody(biStream adapter.QuicBiStream, stream io.Reader, contentLength int) RequestBody {
+func NewRequestBody(
+	biStream adapter.QuicBiStream,
+	stream io.Reader,
+	contentLength int,
+) (RequestBody, error) {
+	if contentLength <= 0 {
+		return nil, fmt.Errorf("received non-positive contentLength in Request Body Constructor")
+	}
+	//if stream == nil || biStream == nil {
+	//	return nil, fmt.Errorf("received nil stream")
+	//}
+
 	return &requestBody{
 		biStream:      biStream,
 		stream:        stream,
 		contentLength: contentLength,
 		bytesToBeRead: contentLength,
-	}
+		violationHandler: sync.OnceFunc(func() {
+			biStream.Close(http3.MessageError)
+		}),
+	}, nil
 }
 
 func (rb *requestBody) Close() error {
@@ -29,16 +47,39 @@ func (rb *requestBody) Close() error {
 	return nil
 }
 
-func (rb *requestBody) Read(dest []byte) (n int, err error) {
-	bufSize := len(dest)
+func (rb *requestBody) Read(b []byte) (n int, err error) {
+
+	if rb.violatedContentLength() {
+		rb.violationHandler()
+		return 0, fmt.Errorf("Content Length Violated in Request Body")
+	}
+
+	bufSize := len(b)
 	if bufSize > rb.bytesToBeRead {
 		bufSize = rb.bytesToBeRead
 	}
 
-	newBuff := dest[:bufSize]
+	newBuff := b[:bufSize]
 	n, err = rb.stream.Read(newBuff)
 
 	rb.bytesToBeRead -= n
 
+	if rb.violatedContentLength() {
+		rb.violationHandler()
+		return n, fmt.Errorf("Content Length Violated in Request Body")
+	}
+
 	return n, err
+}
+
+func (rb *requestBody) violatedContentLength() bool {
+	// TODO: change the reader to an HTTP3 Stream that must have a method to tell
+	//		if there is more data to be read, thus making validation of
+	//		content lenght possible
+	//		Also, BiStream will not be necessary in this case, since it will be
+	//		wrapped under HTTP3 Stream
+	if rb.bytesToBeRead < 0 {
+		return true
+	}
+	return false
 }
